28f2a9155fcb74df567b6120aae4d277330d68d1
Ömer Apaydın <omerapaydin90@gmail.com>
2025-02-15 02:36:39 +0300

    Layout hazırlandı

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+builder.Services.AddControllersWithViews();
+
+
+var app = builder.Build();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.MapControllerRoute(
+    name:"default",
+    pattern:"{controller=Home}/{action=Index}/{id?}"
+);
+
 
 app.Run();
