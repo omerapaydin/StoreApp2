d3a6a36e0da253ba38beee7f1b452e1715d87b20
Ömer Apaydın <omerapaydin90@gmail.com>
2025-02-16 00:23:42 +0300

    Migrations oluşturuldu

---
+++ Data/Concrete/EfCore/IdentityContext.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StoreApp2.Entity;
+
+namespace StoreApp2.Data.Concrete.EfCore
+{
+    public class IdentityContext:IdentityDbContext<ApplicationUser>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options):base(options)
+        {
+            
+       
+        }
+
+         public DbSet<Product> Products => Set<Product>();
+        public DbSet<Category> Categories  => Set<Category>();
+        public DbSet<Comment> Comments => Set<Comment>();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using StoreApp2.Data.Concrete.EfCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<IdentityContext>(options =>{
+    options.UseSqlite(builder.Configuration.GetConnectionString("sql_connection"));
+});
+
 
 
 var app = builder.Build();
